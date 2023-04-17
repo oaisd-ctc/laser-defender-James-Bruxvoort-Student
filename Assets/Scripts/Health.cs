fc55c9e44946cacbc450a742461185532df98014
James-Bruxvoort-Student <112421300+James-Bruxvoort-Student@users.noreply.github.com>
2023-04-17 14:11:25 -0400

    finished up to singleton scorekeeper

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -14,12 +14,14 @@ public class Health : MonoBehaviour
 
     AudioPlayer audioPlayer;
     ScoreKeeper scoreKeeper;
+    LevelManager levelManager;
 
     void Awake() 
     {
         cameraShake = Camera.main.GetComponent<CameraShake>();
         audioPlayer = FindObjectOfType<AudioPlayer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        levelManager = FindObjectOfType<LevelManager>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -55,6 +57,9 @@ public class Health : MonoBehaviour
         if(!isPlayer)
         {
             scoreKeeper.ModifyScore(score);
+        }
+        else{
+            levelManager.LoadGameOver();
         }
         Destroy(gameObject);
     }
